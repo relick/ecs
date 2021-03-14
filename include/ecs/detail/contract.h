c67b3ca42b2f0e54f1f315069bd5413d31ac390f
relick <drrintofen@gmail.com>
2021-03-14 23:00:20 +0000

    avoid contract conflicts and prefer earlier defined contracts

--- include/ecs/detail/contract.h
+++ include/ecs/detail/contract.h
@@ -2,7 +2,11 @@
 #define __CONTRACT
 
 // Contracts. If they are violated, the program is an invalid state, so nuke it from orbit
+#ifndef Expects
 #define Expects(cond) do { ((cond) ? static_cast<void>(0) : std::terminate()); } while(false)
+#endif
+#ifndef Ensures
 #define Ensures(cond) do { ((cond) ? static_cast<void>(0) : std::terminate()); } while(false)
+#endif
 
 #endif // !__CONTRACT
